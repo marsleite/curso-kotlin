2879126062b60832c5b080a9fa898410469c9067
Marcelo <marcelo.leite@mercadolivre.com>
2022-05-10 15:08:23 -0300

    feat: comentarios

---
+++ kotlin/curso_kotlin01/src/main/kotlin/fundamentos/Comentarios.kt
@@ -0,0 +1,12 @@
+package fundamentos
+
+/**
+ *  KDoc, o kotlin suporta.
+ *
+ *  @param args lista de parametros passados por linha de comando
+ */
+fun main() {
+    // Comentando uma linha de comando
+    println("Comentando em uma linha")
+
+}
